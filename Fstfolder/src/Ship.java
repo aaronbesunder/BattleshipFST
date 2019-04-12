946988793c7a15474b87c6328fd8dd48552289d4
matthewboxer <matthew@oasismedia.ca>
2019-04-12 11:57:50 -0400

    Merge branch 'master' of /Users/matthewboxer/IdeaProjects/BattleshipFST with conflicts.

---
+++ Fstfolder/src/Ship.java
@@ -0,0 +1,12 @@
+public class Ship {
+    private int size;
+    private int [] positions;
+
+    public Ship(int s, int [] p) {
+        setSize(s);
+    }
+
+    public void setSize (int size1) {
+
+    }
+}
