11365f9b6d220dd1365a9a9791f9c4d75820e6e5
TMLeafs14 <elapid34@gmail.com>
2019-04-12 12:36:57 -0400

    ok

--- Fstfolder/src/UserInterface.java
+++ Fstfolder/src/UserInterface.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class UserInterface {
+public static void main(String [] args){
+
+    Scanner sc=new Scanner(System.in);
+
+    System.out.println("Hello! Welcome to battleship. If you wish to read the instructions, type in I.");
+    System.out.println("If you wish to start, type in S.");
+
+    String command=sc.nextLine();
+
+    if(command=="I"){
+        System.out.print("On a 10 by 10 board, you will place five ships with different sizes either vertically or");
+        System.out.println();
+    }
+
+}
 }
