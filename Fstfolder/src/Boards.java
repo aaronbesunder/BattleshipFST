d44da57985606ff6450062d8ee15d4d22ec90f29
matthewboxer <matthew@oasismedia.ca>
2019-04-16 12:36:26 -0400

    yay fst

--- Fstfolder/src/Boards.java
+++ Fstfolder/src/Boards.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
-
 public class Boards {
-    public void setAIBoard() {
+    public static void main (String [] args) {
+
         ArrayList<Integer> userBoard = new ArrayList<>();
+
         int verthoriz =(int)(Math.random()*2);
         if(verthoriz==0) {
             int row=(int)((Math.random()*5)+1);
@@ -13,8 +14,9 @@ public class Boards {
             userBoard.add(head+20);
             userBoard.add(head+30);
             userBoard.add(head+40);
-            }
-        if (verthoriz==1){
+        }
+
+        else if (verthoriz==1) {
             int column=(int)((Math.random()*5)+1);
             int row=(int)((Math.random()*10)+1);
             int head= ((column*10)+row);
@@ -23,14 +25,19 @@ public class Boards {
             userBoard.add(head+2);
             userBoard.add(head+3);
             userBoard.add(head+4);
-            }
-
-
-
-
-
         }
+
+        verthoriz =(int)(Math.random()*2);
+        if(verthoriz==0) {
+            int row=(int)((Math.random()*6)+1);
+            int column=(int)((Math.random()*10)+1);
+            int head=((row*10)+column);
+            userBoard.add(head);
+            userBoard.add(head+10);
+            userBoard.add(head+20);
+            userBoard.add(head+30);
+        }
+
+        System.out.println(userBoard);
     }
-
-
 }
